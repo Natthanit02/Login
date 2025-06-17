dac210551dbaf471aefa5d12b2b72885ec0a7857
Natthanit02 <aum.natthanit@gmail.com>
2025-06-17 17:05:30 +0700

    index

--- index.php
+++ index.php
@@ -3,43 +3,52 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 
-    <div class="container mt-5">
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="text-center">Sing IN</h3>
-                    </div>
-                    <div class="card-body">
-                        <div id="login-message-area" class="mb-3"></div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
+        <div class="container">
+            <a href="index.php" class="navbar-brand navbar-brand-custom">AuthSystem</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbraNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a href="index.php" class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="login.html" class="nav-link">Sing In</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="register.html" class="btn btn-primary" role="button">Sign Up</a>
+                    </li>               
+                </ul>
+            </div>
+        </div>
+    </nav>
 
-                        <form id="loginForm">
-                            <div class="mb-3">
-                                <label for="Username" class="form-label">Username</label>
-                                <input type="text" class="form-control" id="username" name="username" required>
-                            </div>
-                             <div class="mb-3">
-                                <label for="Password" class="form-label">Password</label>
-                                <input type="password" class="form-control" id="password" name="password" required>
-                            </div>
-                            <div class="d-grid">
-                                <button type="submit" class="btn btn-success">Sing In</button>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="card-footer text-center">
-                        <p>Do not have an account yet? <a href="register.html">Sign UP</a></p>
-                    </div>
+    <div class="main-content">
+        <div class="container">
+            <div class="hero-section">
+                <h1>Welcome to our web</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur, placeat odio! Numquam mollitia consequuntur, sapiente doloremque ullam perspiciatis fugiat nobis!</p>
+                <div>
+                    <a href="login.html" class="btn btn-success btn-lg btn-custom">Sign In</a>
+                    <a href="register.html" class="btn btn-success btn-lg btn-custom">Create an account</a>
                 </div>
             </div>
         </div>
     </div>
+
+    <footer class="footer mt-auto">
+        <div class="container">
+            <span>&copy; <?php echo date("Y"); ?> AuthSystem</span>
+        </div>
+    </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
